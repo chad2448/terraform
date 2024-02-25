c8308dae72fa426cd5bf98b031cd132fe71297f0
chad <cduren32@gmail.com>
2024-02-25 12:30:18 -0500

    add

--- proxmox/main.tf
+++ proxmox/main.tf
@@ -23,19 +23,18 @@ resource "proxmox_lxc" "debian" {
     start        = true
     tags         = "terraform"
     unprivileged = true
+    nameserver   = "192.168.1.1"
     ssh_public_keys = <<-EOT
      ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDLC/aJpuicQOTSBXaFnUuzZwgC3DP8qByO9SdItZrMz3LNh1jKtvSQE0OHYhJTxWf0xJtziZV1BlvOn+TIH4eXXytxNZNZbhv6TybHNa+M7PAPEW6QXlkPUyzqhjvGdoklbNQ49HIKEflMIPx1UCxM8fYifnXGnTVzcIVgjMowu2puIL1WN/RFmpWWROqJXddZcWQvaNaNznCcyjDzrJqrKGE1WBEcmV0e2PRmW+0QDG4JyFCfurIcZhnRFD4LA02yLub9aXLeWmQ8VJRFPv/TnvmF8D89ElwagfNAkCIM0q36biueJxTNQYXjKem9dVMCmHZn4fXhiQ4ojGK/k3aHeUrUPl27ahnW/T3YcM9japdNPDt5s4UMHzNTaulEo5OTNEthOXEK+6TM2PC250Va1ljoxXXRjUs5lDs/qTRdJpn9Leb84q/SJ6E3ZCWDfNqyirQRdhWgErWf6bLd/kFnES4nvLK64MShn/Hrbq9sxVZSBsduEf3+PBUhakW0Vk8= chad@OfficePC
   EOT
 
-
-
   network {
     name   = "eth0"
     bridge = "vmbr0"
-    # ip     = "dhcp"
-    ip     = "192.168.1.238/24"
+    ip     = "dhcp"
     tag    = 5
   }
+  
   rootfs {
     storage = "local-lvm"
     size    = "8G"
